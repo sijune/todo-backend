8f2d3c182a8838715eb280ddd1f2c460616abc5e
sijune <sijune0525@gmail.com>
2022-01-15 12:04:55 +0900

    deploy

--- src/main/java/com/sijune/todobackend/config/WebMvcConfig.java
+++ src/main/java/com/sijune/todobackend/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://prod-todo-frontend2.ap-northeast-2.elasticbeanstalk.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
